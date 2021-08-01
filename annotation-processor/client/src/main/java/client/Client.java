80d4cd16f74eb79557bc88f1ba66afc18784ec23
Marshal <marshal960818@gmail.com>
2021-08-01 20:58:26 +0800

    feat:annotation processor

--- annotation-processor/client/src/main/java/client/Client.java
+++ annotation-processor/client/src/main/java/client/Client.java
@@ -2,20 +2,23 @@ package client;
 
 import java.lang.reflect.Method;
 
+import product.Animal;
+import product.AnimalFactory;
+
 /**
  * @author Marshal
  * @date 2021/8/1
  */
 public class Client {
     public static void main(String[] args) throws Exception {
-        Class<?> clazz = Class.forName("client.product.AnimalFactory");
+        Class<?> clazz = Class.forName("product.AnimalFactory");
         Object o = clazz.newInstance();
         Method method = clazz.getDeclaredMethod("create", String.class);
 
         Object dog = method.invoke(o, "dog");
 
-//        AnimalFactory animalFactory = new AnimalFactory();
-//        animalFactory.create("dog");
+        AnimalFactory animalFactory = new AnimalFactory();
+        Animal animal = animalFactory.create("dog");
 
         System.out.println(222);
     }
